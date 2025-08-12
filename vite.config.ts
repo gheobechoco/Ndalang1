1fd29f5d206c910bc4d08b92b00dd88398c32e1d
gheo bechoco <moueletgeoff@gmail.com>
2025-08-12 06:22:13 +0200

    ajout de vercel.json et npmrc et variable dans vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Charge toutes les variables d'environnement commençant par VITE_
-  const env = loadEnv(mode, process.cwd(), '');
+  // Charge uniquement les variables VITE_*
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
 
   return {
     plugins: [react()],
     define: {
-      // Expose les variables d'environnement au code client
+      // N'expose que les variables VITE_ au client
       'import.meta.env': JSON.stringify(env),
     },
     server: {
       watch: {
-        ignored: [
-          '**/.wine/**',
-          '**/node_modules/**'
-        ]
+        ignored: ['**/.wine/**', '**/node_modules/**']
       }
     }
   };
